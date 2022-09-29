bfdaa14aceda2af399adce76bf4e94562d814154
Greg Hayes <gregphayea1@gmail.com>
2022-09-29 09:55:31 +0100

    Add project files.

---
+++ Sudoku/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+class Sudoku
+{
+    int[,] puzzle = new int[9, 9];
+    Sudoku()
+    {
+        Randomise();
+    
+    }
+    
+    public void Randomise()     //this will randomise the order of numbers in the sudoku board
+    {
+        Random r1 = new Random();
+        for (int i = 0; i < 9; i++)  //fill out a row
+        {
+            for(int j = 0; j < 9; j++)  //fill out a column
+            {
+               int temp = r1.Next(10);
+               
+                
+            }
+
+        }
+
+    }
+
+    //these are the functions that check the validity of the number to be put in the box
+    public bool RowCheck() 
+    {
+        
+        return true;
+    }
+
+    public bool ColCheck() 
+    { 
+        
+        return true; 
+    }
+
+    public bool BoxCheck()
+    { 
+        
+        return true; 
+    }
+}
